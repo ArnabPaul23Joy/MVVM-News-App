31863b3e0493fcd4733211459befbbde4dacefa6
arnab <arnab.joy@samsung.com>
2025-07-01 02:41:01 -0400

    retrofit builder created. working on room db

--- app/src/main/java/com/example/mvvmnewsapplatest/network/api/NewsApi.kt
+++ app/src/main/java/com/example/mvvmnewsapplatest/network/api/NewsApi.kt
@@ -17,4 +17,14 @@ interface NewsApi {
         @Query ("apiKey")
         apiKey: String = API_KEY
     ): Response<NewsResponse>
+
+    @GET ("v2/everything")
+    suspend fun searchForNews(
+        @Query ("q")
+        searchQuery: String,
+        @Query ("page")
+        pageNumber: Int,
+        @Query ("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
 }
