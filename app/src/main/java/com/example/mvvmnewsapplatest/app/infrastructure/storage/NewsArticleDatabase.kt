78330a129fab120dbe115210152fc1bb88609994
arnab <arnab.joy@samsung.com>
2025-07-02 08:19:34 -0400

    recycler view created

--- app/src/main/java/com/example/mvvmnewsapplatest/app/infrastructure/storage/NewsArticleDatabase.kt
+++ app/src/main/java/com/example/mvvmnewsapplatest/app/infrastructure/storage/NewsArticleDatabase.kt
@@ -4,11 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.mvvmnewsapplatest.app.infrastructure.storage.dao.ArticleDao
-import com.example.mvvmnewsapplatest.app.model.ArticleEntity
+import com.example.mvvmnewsapplatest.app.model.Article
 import com.example.mvvmnewsapplatest.app.infrastructure.network.constants.Constants
 
 @Database (
-    entities = [ArticleEntity::class],
+    entities = [Article::class],
     version = Constants.Companion.VERSION_NUMBER
 )
 @TypeConverters (DataTypeConverter::class)
